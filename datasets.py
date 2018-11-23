ee5bec8da9898d31db2b24cd0a6480676fd6333c
Amir Ziai <arziai@gmail.com>
2018-11-22 18:01:38 -0800

    docs as a param

--- datasets.py
+++ datasets.py
@@ -8,16 +8,16 @@ DATASET_PATH = 'resources/datasets/'
 MAX_DOCS = 2000
 
 
-def get_bbc() -> List[str]:
+def get_bbc(docs=MAX_DOCS) -> List[str]:
     docs = [line[1] for i, line in enumerate(csv.reader(open(f"{DATASET_PATH}bbc-text.csv"), delimiter=',')) if i > 0]
-    return docs[:MAX_DOCS]
+    return docs[:docs]
 
 
 # articles1.csv from https://www.kaggle.com/snapcrack/all-the-news#articles1.csv.
-def get_all_the_news() -> List[str]:
-    data = pd.read_csv(f"{DATASET_PATH}articles1.csv", nrows=MAX_DOCS)
+def get_all_the_news(docs=MAX_DOCS) -> List[str]:
+    data = pd.read_csv(f"{DATASET_PATH}articles1.csv", nrows=docs)
     return data['content'].tolist()
 
 
-def get_20newsgroups() -> List[str]:
-    return fetch_20newsgroups(shuffle=True, remove=('headers', 'footers', 'quotes')).data[:MAX_DOCS]
+def get_20newsgroups(docs=MAX_DOCS) -> List[str]:
+    return fetch_20newsgroups(shuffle=True, remove=('headers', 'footers', 'quotes')).data[:docs]
