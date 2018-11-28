Fix bug with score scaling

interactive.py
@@ -8,7 +8,7 @@ from scipy.spatial.distance import cdist
 from sklearn.metrics import silhouette_score, silhouette_samples
 
 import cmeans as Fuzzy
-
+from app import scale_score
 
 # confusion matrix
 def computeX2(attrVals, clusters, data, N):
@@ -146,7 +146,7 @@ def icluster(data, terms, userFeedbackTerm, k, userU=-1):
     for i, label in enumerate(IDX):
         ith_cluster_silhouette_values = sample_silhouette_values[IDX == label]
         avg = numpy.mean(ith_cluster_silhouette_values)
-        scores[str(label)] = 50 + (50 / (pow(10, (1.0 / 3)))) * pow(10.0 * avg, (1.0 / 3))
+        scores[str(label)] = scale_score(avg)
     attrVals = numpy.empty([M, k], dtype=float)
     computeX2(attrVals, clusters, data, N)
     for p in range(k):